377e3e462744abfd828edb4783a78a7b47d28a00
Antony Memia <antony@olywave.com>
2020-10-21 10:35:37 +0300

    flash messages, saving data to db

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -8,20 +11,37 @@ const app = express();
 const db = require('./config/keys').MongoURI;
 
 //Connect to Mongo
-mongoose.connect(db, {useNewUrlParser: true})
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
 // Ejs
+app.use(expressLayouts);
 app.set('view engine', 'ejs');
  
 
 //Bodyparser
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 
+//Express session
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+ }
+));
 
+//Connect flash
+app.use(flash())
 
-app.use(expressLayouts);
+//Global Vars
+app.use((req, res, next) => {
+        res.locals.success_msg = req.flash('success_msg');
+        res.locals.error_msg = req.flash('error_msg');
+    next();
+
+})
+
 
 //routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//User model 
+const User = require('../models/User');
 
 //Login page
 router.get('/login', function (req, res) {
@@ -35,7 +39,47 @@ router.post('/register', (req, res) => {
       password2
     });
   } else {
-    res.send('pass');
+    //validation pass
+    User.findOne({ email:email})
+      .then(user => {
+        if(user) {
+              //user exists
+              errors.push({ msg: 'Email is already registered'});
+              res.render('register', {
+                errors,
+                name,
+                email,
+                password,
+                password2
+              });
+        } else {
+          const newUser = new User ({
+              name,
+              email,
+              password
+          })
+
+         //console.log(newUser);
+         // res.send('hello');
+          bcrypt.genSalt(10, (err, salt) =>
+             bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if(err) throw err;
+              //Set password to hashed
+              newUser.password = hash;
+              //Save user
+              newUser.save()
+                .then(user => {
+                  req.flash('success_msg', 'You are now registered and can log in');
+                  res.redirect('/users/login');
+                })
+                .catch(err => co);
+
+          }))
+
+
+        }
+        
+      })
   }
 
 })
