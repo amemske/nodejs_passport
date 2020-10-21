7d8d4847d4e9cdc461814412ad8068c947fa4004
amemske <olywave@gmail.com>
2020-10-21 10:35:37 +0300

    flash messages, saving data to db

--- models/User.js
+++ models/User.js
@@ -3,25 +3,24 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name:{
         type: String,
-        required: True
+        required: true
     },
     email:{
         type: String,
-        required: True
+        required: true
     },
     password:{
         type: String,
-        required: True
+        required: true
     },
-    name:{
+    date:{
         type: Date,
         default: Date.now
     }
 
 });
 
-const User = mongoose.model('User, UserSchema');
-
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
 
 
